154a8c0430e7bcd3ac6645fe3d510799139deed0
Karthik Ramachandran <karthikeyan.ramachandran@corpay.com>
2026-04-02 21:47:03 -0400

    chore: add alembic merge migration for role + domain table branches

---
+++ apps/api/app/alembic/versions/ab178426267c_merge_role_and_domain_tables.py
@@ -0,0 +1,25 @@
+"""merge_role_and_domain_tables
+
+Revision ID: ab178426267c
+Revises: a1b2c3d4e5f6, f1a2b3c4d5e6
+Create Date: 2026-04-02 21:43:12.501058
+
+"""
+from alembic import op
+import sqlalchemy as sa
+import sqlmodel.sql.sqltypes
+
+
+# revision identifiers, used by Alembic.
+revision = 'ab178426267c'
+down_revision = ('a1b2c3d4e5f6', 'f1a2b3c4d5e6')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
